1cd3ce98028f01e4c9e2e0c0e5396b34c07327bb
ginanjar busiri <gbusiri@gmail.com>
2016-10-02 16:02:31 +0700

    first commit, add project netbeans

---
+++ machine_learning_id3/src/machine_learning_id3/Machine_learning_id3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machine_learning_id3;
+
+/**
+ *
+ * @author ginanjarbusiri
+ */
+public class Machine_learning_id3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
